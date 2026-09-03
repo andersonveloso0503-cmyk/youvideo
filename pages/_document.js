2e49c8621a1376399697e53372a6bb637e4c9103
andersonveloso0503-cmyk <andersonveloso0503@gmail.com>
2026-09-03 18:21:32 -0300

    Add files via upload

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta
+          name="tiktok-developers-site-verification"
+          content="UT74xCdNdBJiPXf0bQ5vBYL4BmClQnsj"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
